49dd89b2b89c1160412cba36aac0a34471ee21bd
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-15 16:45:03 -0400

    House location screen implementation

--- housing-location/src/app/housing-location/housing-location.component.ts
+++ housing-location/src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation'
 @Component({
   selector: 'app-housing-location',
   standalone: true,
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
